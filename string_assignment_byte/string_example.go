c63461d5b921b3490197256fd2ad30c3b42db52a
isfpcn <454919140@qq.com>
2019-04-22 16:07:30 +0800

    string to byte example

---
+++ string_assignment_byte/string_example.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b [64]byte
+	s := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	// 这么赋值肯定是错误的 类型不同
+	//b = s
+
+	//遍历赋值 这种是可以的
+	//for i := 0; i < len(s); i++ {
+	//	b[i] = s[i]
+	//}
+	nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8
+	//用copy方式，前提是必须指定[]byte的len
+	copy(b[:],s)
+
+	fmt.Println(b)
+
+	name := "Hello，中国。"
+
+	length := len(name)
+
+	bb := make([]byte,length)
+
+	copy(bb,name)
+
+	fmt.Println(string(bb))
+}
